b647bdf48d04eeafd13d65566d9663d65f3aa23f
khach <asmaryan02@mail.ru>
2021-12-21 14:42:24 +0400

    Dog

--- src/src/basic/oop/DogTest.java
+++ src/src/basic/oop/DogTest.java
@@ -3,6 +3,6 @@ package basic.oop;
 public class DogTest {
     public static void main(String[] args) {
         Dog dog = new Dog( 1);
-        System.out.println(dog.sayHaf());
+        dog.getVoice();
     }
 }
